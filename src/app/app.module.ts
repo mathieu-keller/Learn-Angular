bbfafd166e018453373ceab7e8f310819ee0b0c0
Mathieu Keller <mathieu.keller@sda.se>
2024-04-05 10:19:35 +0200

    make the main branch as default angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 
-import {ToastWarningComponent} from './toast-warning/toast-warning.component';
-import {ToastSucceedComponent} from './toast-succeed/toast-succeed.component';
-import {ServerComponent} from './server/server.component';
-import {ServersComponent} from './servers/servers.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ToastWarningComponent,
-    ToastSucceedComponent,
-    ServerComponent,
-    ServersComponent
   ],
   imports: [
     BrowserModule,
